0831dd9e06b529a035679a170503722c0d222e6b
Nigel <33954651+N-itotia@users.noreply.github.com>
2019-05-31 10:50:09 +0300

    Update screencaster-3a.php

--- api-work/rats/screencaster-3a.php
+++ api-work/rats/screencaster-3a.php
@@ -1,25 +1,34 @@
+<!DOCTYPE html>
+    <head>
+        <title>API RAT 1</title>
+    </head>
 
-<?php
-$balance1 = 100+200+300+678;
-$balance2 = 530+670+3+20;
-$balance3 = 3000+39+49+98;
-
-
-$shop = array( array("company"=>"archeNland Limited", "Amounts"=>"100,-1,200,300,-700,678" , "Debits"=>"1,700", "Balance"=>$balance1),
-               array("company"=>"Narnia Gmb", "Amounts"=>"530,-23,670,3,-1299" , "Debits"=>"1299,3", "Balance"=>$balance2),
-               array("company"=>"Loneislands Pty", "Amounts"=>"-7568,39,49,-37,3000,98" , "Debits"=>"37,7568", "Balance"=>$balance3) 
-             ); 
-?>
-
-<?php if (count($shop) > 0): ?>
+    <body>
+        <?php 
+            $archenland_balance = 100 + 200 + 300 + 678;
+            $narnia_gmb = 530 + 670 + 3;
+            $loneislands_pty = 39 + 49 + 3000 + 98;
+            $archenland_debits = "1, 700";
+            $narnia_debits = "1299, 3";
+            $loneislands_debits = "7568, 37";
+            
+                $companies = array(array("company"=>"Archenland Ltd.", "Amounts"=>"100,-1,200,300,-700,678" , "Debits"=>$archenland_debits, "Balance"=>$archenland_balance),
+                array("company"=>"Narnia Gmb.", "Amounts"=>"530,-23,670,3,-1299" , "Debits"=>$narnia_debits, "Balance"=>$narnia_gmb),
+                array("company"=>"Loneislands Pty", "Amounts"=>"-7568,39,49,-37,3000,98" , "Debits"=>$loneislands_debits, "Balance"=>$loneislands_pty));
+                
+                
+                
+        ?>       
+        
+              <?php if (count($companies) > 0): ?>
 <table>
   <thead>
     <tr>
-      <th><?php echo implode('</th><th>', array_keys(current($shop))); ?></th>
+      <th><?php echo implode('</th><th>', array_keys(current($companies))); ?></th>
     </tr>
   </thead>
   <tbody>
-<?php foreach ($shop as $row): array_map('htmlentities', $row); ?>
+<?php foreach ($companies as $row): array_map('htmlentities', $row); ?>
     <tr>
       <td><?php echo implode('</td><td>', $row); ?></td>
     </tr>
